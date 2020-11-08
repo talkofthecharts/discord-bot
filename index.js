2b2cdd71b82398962b6ca29b11a88af54e52b89f
TOTC <talkofthecharts@gmail.com>
2020-11-08 15:03:23 +1100

    fix: run code outside function

--- index.js
+++ index.js
@@ -8,18 +8,16 @@ const TOKEN = process.env.TOKEN;
 bot.login(TOKEN);
 
 function Spotify() {
-  function initialStep() {
-    axios
-      .get("https://spotifycharts.com/regional/us/daily/latest")
-      .then((response) => {
-        const $ = cheerio.load(response.data);
-        const latestDate = $(
-          '.responsive-select[data-type="date"] > ul > li'
-        ).text();
-        const [month, day, year] = latestDate.split("/");
-        fetchLatestChart({ year, month, day });
-      });
-  }
+  axios
+    .get("https://spotifycharts.com/regional/us/daily/latest")
+    .then((response) => {
+      const $ = cheerio.load(response.data);
+      const latestDate = $(
+        '.responsive-select[data-type="date"] > ul > li'
+      ).text();
+      const [month, day, year] = latestDate.split("/");
+      fetchLatestChart({ year, month, day });
+    });
 
   function fetchLatestChart({ year, month, day }) {
     axios
